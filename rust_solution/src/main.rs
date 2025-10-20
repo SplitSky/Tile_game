f6b8415af8f85e6fc4f410d0683014911dd485c9
SplitSky <opop.neska60@gmail.com>
2025-10-20 19:05:59 +0100

    feat: play game skeleton code

--- rust_solution/src/main.rs
+++ rust_solution/src/main.rs
@@ -33,6 +33,7 @@ fn gen_all_combs(tiles: Vec<i8>, limit: i8) -> Vec<Vec<i8>> {
             return true;
         })
         .collect();
+    all_combs.sort();
     return all_combs;
 }
 
@@ -45,9 +46,24 @@ fn print_Vector(vector: Vec<Vec<i8>>) {
     }
 }
 
+fn play_turn() -> i16 {
+    let score: i16 = 0;
+    /*
+    Conditions to stop playing:
+    1. All tiles flipped
+    2. Number can't be made from the combination
+    if conditions met then add up tiles and return score
+    else keep playing
+    tile flipped = removed
+    */
+}
+
 fn main() {
     // let board: Vec<i8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     // for the tiles generate combinations to pick from
     let tiles: Vec<i8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     let all_combs = gen_all_combs(tiles, 12);
+    print_Vector(all_combs);
+
+    // play the game loop
 }
