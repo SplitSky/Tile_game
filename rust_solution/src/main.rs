ac13e64ee30c12bfd1727fd741cb8388f50548ad
SplitSky <opop.neska60@gmail.com>
2025-10-20 18:29:39 +0100

    feat: added add combination function with limit to allow for generating game appropriate tile combinations

--- rust_solution/src/main.rs
+++ rust_solution/src/main.rs
@@ -1,24 +1,53 @@
-struct Combination {
-    tiles: Vec<i8>,
-}
-
-impl Combination {
-    fn sort_tiles(Self) {
-        tiles.sort();
-    }
-}
-
 fn add_combs(tile: i8, combinations: Vec<Vec<i8>>) -> Vec<Vec<i8>> {
-    // copy combinations as mutable
-    let mut comb_copy = Vec::new();
-    comb_copy = combinations.clone();
-    // append the tile to every entry in comb_copy
-    for mut combination in comb_copy {
-        combination.push(tile);
+    let new_combs: Vec<Vec<i8>> = combinations
+        .iter()
+        .map(|c| {
+            let mut c = c.clone();
+            c.push(tile);
+            c
+        })
+        .collect();
+    let mut combined = combinations.clone();
+    combined.push(vec![tile]);
+    combined.extend(new_combs);
+    combined
+}
+
+fn gen_all_combs(tiles: Vec<i8>, limit: i8) -> Vec<Vec<i8>> {
+    // make empty set
+    let mut all_combs: Vec<Vec<i8>> = Vec::new();
+    for tile in tiles {
+        all_combs = add_combs(tile, all_combs);
     }
-    comb_copy
+    // filter based on limit adding up to less than 12
+    all_combs = all_combs
+        .into_iter()
+        .filter(|x| {
+            let mut sum: i8 = 0;
+            for entry in x.iter() {
+                sum += entry;
+                if sum > limit {
+                    return false;
+                }
+            }
+            return true;
+        })
+        .collect();
+    return all_combs;
+}
+
+fn print_Vector(vector: Vec<Vec<i8>>) {
+    for temp in vector {
+        for tile in temp {
+            print!("{}", tile);
+        }
+        println!("{}", " ");
+    }
 }
 
 fn main() {
-    let board: Vec<i8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    // let board: Vec<i8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    // for the tiles generate combinations to pick from
+    let tiles: Vec<i8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let all_combs = gen_all_combs(tiles, 12);
 }
