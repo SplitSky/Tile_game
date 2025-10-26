199f927033b196be5de92241253f08981099819c
SplitSky <opop.neska60@gmail.com>
2025-10-26 18:22:44 +0000

    added strategy branching todo

--- rust_solution/src/main.rs
+++ rust_solution/src/main.rs
@@ -40,6 +40,7 @@ fn gen_all_combs(tiles: Vec<i8>, limit: i8) -> Vec<Vec<i8>> {
 }
 
 fn gen_all_combs_for_roll(tiles: &Vec<i8>, roll: i8) -> Vec<Vec<i8>> {
+    // doesn't filter the tiles based on possibilities
     let mut all_combs: Vec<Vec<i8>> = Vec::new();
     for &tile in tiles.iter() {
         all_combs = add_combs(tile, all_combs);
@@ -48,6 +49,22 @@ fn gen_all_combs_for_roll(tiles: &Vec<i8>, roll: i8) -> Vec<Vec<i8>> {
         .into_iter()
         .filter(|x| x.iter().sum::<i8>() == roll)
         .collect()
+}
+
+fn filter_combs_on_tiles(combinations: Vec<Vec<i8>>, tiles: &Vec<i8>) -> Vec<Vec<i8>> {
+    //tiles.iter().filter(|x| !combinations)
+    let filtered_combs: Vec<Vec<i8>> = combinations
+        .into_iter()
+        .filter(|x| {
+            for tile in x.iter() {
+                if !tiles.contains(tile) {
+                    return false;
+                }
+            }
+            return true;
+        })
+        .collect();
+    return filtered_combs;
 }
 
 fn print_vector(vector: &Vec<Vec<i8>>) {
@@ -76,30 +93,69 @@ fn roll_2d6() -> i8 {
     result
 }
 
-fn pick_combination_strat(combinations: Vec<Vec<i8>>, strat: i8) -> Vec<i8> {
+fn strat_1(combinations: &Vec<Vec<i8>>, tiles: &Vec<i8>) -> Vec<i8> {
+    // select based on tree branching count
+    // i.e. for each comb generate combinations. Count and make a dict.
+    // Then pick one with the largest count of possible combinations
+    let combs = &combinations;
+    let mut boards: Vec<Vec<i8>> = vec![];
+    let mut scores: Vec<i16> = Vec::new();
+    for combination in combs.into_iter() {
+        // generate a board for each selection
+        let mut board = flip_combination(combination, tiles);
+        boards.push(board);
+    }
+    // evaluate the board combination counts
+    let lowest = 1000;
+    let best_comb = 
+    for board in boards.into_iter() {
+        let sim_combs = gen_all_combs(board, 12);
+        scores.push(sim_combs.len());
+    }
+}
+
+struct board_comb{
+    board: Vec<i8>,
+    choice: Vec<i8>,
+    score: i8 = board.iter().sum::<i8>(),
+}
+impl board_comb {
+    fn new(board: Vec<i8>, choice: Vec<i8>) {
+
+    }
+}
+
+fn pick_combination_strat(combinations: Vec<Vec<i8>>, strat: i8, tiles: &Vec<i8>) -> Vec<i8> {
     // pick the first combination
     print_vector(&combinations);
-    let choice: Vec<i8> = combinations[0].clone();
-    choice
+    if strat == 0 {
+        return combinations[combinations.len() - 1].clone();
+    } else if strat == 1 {
+        return strat_1(&combinations, tiles);
+    } else {
+        return combinations[0].clone();
+    }
 }
 
 fn play_turn() -> i8 {
     let mut board: Vec<i8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mut keep_playing = true;
     let mut roll: i8 = roll_2d6();
-    let mut choices = gen_all_combs_for_roll(&board, roll);
+    let mut choices: Vec<Vec<i8>>;
     let mut score: i8 = 0;
 
     println!("{}", roll);
 
     while keep_playing == true {
         roll = roll_2d6();
-        choices = gen_all_combs_for_roll(&board, roll);
+        choices = gen_all_combs_for_roll(&board, roll); // generate tile combinations for a given
+        // roll
+        choices = filter_combs_on_tiles(choices, &board);
         if choices.is_empty() {
             keep_playing = false;
             score = board.iter().sum::<i8>();
         } else {
-            let choice = pick_combination_strat(choices.clone());
+            let choice = pick_combination_strat(choices.clone(), 0, &board);
             board = flip_combination(choice, &board);
         }
     }
@@ -107,7 +163,6 @@ fn play_turn() -> i8 {
 }
 
 fn main() {
-    // let board: Vec<i8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     // for the tiles generate combinations to pick from
     let tiles: Vec<i8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     let all_combs = gen_all_combs(tiles, 12);
